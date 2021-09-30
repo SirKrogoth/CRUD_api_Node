b1f9bf64f155244e5c248339ea737f36d8844a7a
joao rafael fagundes menezes <menezes.jrafael@gmail.com>
2021-09-30 16:49:55 -0300

    Inserindo uso do Joi

--- routes/users.js
+++ routes/users.js
@@ -17,20 +17,30 @@ router.get('/:id', function(_req, _res){
 });
 
 //inserindo dados na base
-router.post('/', function(req, res){
-  const { error } = userSchema.validate(req.body);//Pegando o elemento complexo, e capturando apenas os seus erros e colocando em uma constante
-
-  if(error)
-    return res.status(422).json({error: error.details});
-
+router.post('/', validationMiddleware, function(req, res){
   db.insertUser(req.body);
   res.status(201).json(require(_usersJsonCaminho));//retorna no json o user
 });
 
-router.put('/:id', function(req, res){
+function validationMiddleware(req, res, next){
+  const { error } = userSchema.validate(req.body);//Pegando o elemento complexo, e capturando apenas os seus erros e colocando em uma constante
+
+  if(error)
+    return res.status(422).json({error: error.details});
+  else
+    next();
+}
+
+router.put('/:id', validationMiddleware, function(req, res){
   const id = req.params.id;
   db.updateUser(id, req.body);
   res.status(200).json(require(_usersJsonCaminho))
+});
+
+router.patch('/:id', function (req, res){
+  const id = req.params.id;
+  const user = db.updateUser(id, req.body, false);
+  res.status(200).json(user);
 });
 
 router.delete('/:id', function(req, res){
